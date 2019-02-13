c9f32240dd10de1e762d030390d1c4ba07d2a1c6
PakAlyona <PakAlyona@users.noreply.github.com>
2019-02-13 23:41:34 +0600

    Finally it works

---
+++ Week_2/Task_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+
+namespace Task_1
+{
+    class Program
+    {
+
+        public static bool Palin(string s)
+        {
+
+            int i = 0;
+            int j = s.Length - 1;
+
+            while (i < j)
+            {
+                if (s[i] != s[j])         // проверяем строку на палиндромность через реверс
+                    return false;
+
+                i++;
+                j--;
+            }
+
+            return true;
+
+        }
+
+
+        static void Main(string[] args)
+        {
+
+            FileStream fs = new FileStream(@"C:\Texts\input_t1.txt", FileMode.Open, FileAccess.Read);       //открываем файл, указывая до него путь
+            StreamReader sr = new StreamReader(fs);
+
+            string str = sr.ReadLine();     // переводим данные с файла в строку
+
+            if (Palin(str))     //чекаем на палиндромность
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+                Console.WriteLine("No");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Week_2/Task_2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static void ReadF()
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            String s = sr.ReadToEnd();
+            String[] arr = s.Split();
+            int sum = 0;
+            foreach (String a in arr)
+            {
+                sum += int.Parse(a);
+            }
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("output.txt");
+            sw.WriteLine(sum);
+            sw.Close();
+        }
+
+        static bool Prime(int a) // создаем логическую функцию, которая определяет простоту числа
+        {
+            if (a == 1)
+            {
+                return false;
+            }
+            for (int i = 2; i <= Math.Sqrt(a); i++)
+            {
+                if (a % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            ReadF();
+            Prime();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
