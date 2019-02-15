5702564b9f0d68ac47afda1dc963b509a4d7c3c1
PakAlyona <PakAlyona@users.noreply.github.com>
2019-02-15 21:58:00 +0600

    That was hard

---
+++ Week_2/Task_3/Task_3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static void Spisok (int floor)
+        {
+            for (int i = 0; i < floor; i++)
+            {
+                Console.Write("   ");      // чтоб красиво всё разделялось
+            }
+        }
+
+        public static void Direc(DirectoryInfo dir, int floor) 
+        {
+            foreach (FileInfo f in dir.GetFiles())      // не уверена че она делает, но кажется вызывает предыдущие дериктории
+            {
+                Spisok(floor);     // разделяем, властвуем, унижаем
+                Console.WriteLine(f.Name);
+            }
+
+            foreach (DirectoryInfo d in dir.GetDirectories())
+            {
+                Spisok(floor);     // разделяем, властвуем, унижаем
+                Console.WriteLine(d.Name);
+                Direc(d, floor + 1);        // рекурсируем
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo directory = new DirectoryInfo("C:/Users/User/desktop/Study/University");      // выбираем папку
+
+            Direc(directory, 0);        // призываем функцию
+
+            Console.ReadKey();
+        }
+    }
+}
