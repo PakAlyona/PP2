60667d9d17717192eb0d0073d0d93faa0a7a0a47
PakAlyona <PakAlyona@users.noreply.github.com>
2019-02-16 01:45:22 +0600

    не получилось

---
+++ Week_3/Program.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Week_3
+{
+    class FarManager
+    {
+        public bool dir;
+        public int cursor;
+        public string path;
+        public int sz;
+        public bool hidden;
+        DirectoryInfo directory = null;
+        FileSystemInfo currentFs = null;
+
+        public FarManager()
+        {
+            cursor = 0;
+        }
+
+        public FarManager(string path)
+        {
+            this.path = path;
+            cursor = 0;
+            directory = new DirectoryInfo(path);
+            sz = directory.GetFileSystemInfos().Length;
+            hidden = true;
+        }
+
+        public void Color(FileSystemInfo fs, int index)
+        {
+            if (cursor == index)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                currentFs = fs;
+            }
+            else if (fs.GetType() == typeof(DirectoryInfo))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Magenta;
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+        }
+
+        public void Show()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            directory = new DirectoryInfo(path);
+            FileSystemInfo[] fs = directory.GetFileSystemInfos();
+            for (int i = 0, k = 0; i < fs.Length; i++)
+            {
+                if (hidden == false && fs[i].Name[0] == '.')
+                {
+                    continue;
+                }
+                Color(fs[i], k);
+                Console.WriteLine(fs[i].Name);
+                k++;
+            }
+        }
+        public void Up()
+        {
+            cursor--;
+            if (cursor < 0)
+                cursor = sz - 1;
+        }
+        public void Down()
+        {
+            cursor++;
+            if (cursor == sz)
+                cursor = 0;
+        }
+
+        public void CalcSz()
+        {
+            directory = new DirectoryInfo(path);
+            FileSystemInfo[] fs = directory.GetFileSystemInfos();
+            sz = fs.Length;
+            if (hidden == false)
+                for (int i = 0; i < fs.Length; i++)
+                    if (fs[i].Name[0] == '.')
+                        sz--;
+        }
+
+        public void Start()
+        {
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+            while (consoleKey.Key != ConsoleKey.Escape)
+            {
+                CalcSz();
+                Show();
+                consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.UpArrow)
+                    Up();
+                if (consoleKey.Key == ConsoleKey.DownArrow)
+                    Down();
+                if (consoleKey.Key == ConsoleKey.RightArrow)
+                {
+                    hidden = false;
+                    cursor = 0;
+                }
+                if (consoleKey.Key == ConsoleKey.LeftArrow)
+                {
+                    cursor = 0;
+                    hidden = true;
+                }
+                if (consoleKey.Key == ConsoleKey.Enter)
+                {
+                    if (currentFs.GetType() == typeof(DirectoryInfo))
+                    {
+                        cursor = 0;
+                        path = currentFs.FullName;
+                    }
+                }
+                if (consoleKey.Key == ConsoleKey.Backspace)
+                {
+                    cursor = 0;
+                    path = directory.Parent.FullName;
+                }
+            }
+        }
+
+    }
+    // до этого момента я копипастнула у препода потому что хотела спать
+
+    public static void Delete(string directoryName, bool checkDirectiryExist)
+    {
+        if (Directory.Exists(directoryName))
+            Directory.Delete(directoryName, true);
+        else if (checkDirectiryExist)
+            throw new SystemException("Directory you want to delete is not exist");
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/User/desktop/PP2/Week_3/source";
+            FarManager farManager = new FarManager(path);
+            farManager.Start();
+        }
+    }
+}
