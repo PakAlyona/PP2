85bc4b6744c6c4f0d15cb90dc3c4f9fa8fea2459
PakAlyona <PakAlyona@users.noreply.github.com>
2019-02-15 23:39:07 +0600

    It was easy so probably it isn't right

---
+++ Week_2/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {        
+        
+        public static void Main(string[] args) 
+        {
+                StreamWriter task4 = new StreamWriter(@"C:/Texts/path1/Task4.txt");     // создаем файл в папке1
+                task4.WriteLine("Nadeus eto rabotaet");     // пишем туда текстик
+                task4.Close();
+                File.Copy(@"C:/Texts/path1/Task4.txt", @"C:/Texts/path2/Task4.txt");        // копируем из папки1 в папку2
+                File.Delete(@"C:/Texts/path1/Task4.txt");       // удаляем в папке1
+
+        }
+    }
+}
