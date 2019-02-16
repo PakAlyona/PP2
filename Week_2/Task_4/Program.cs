df3c599f3eb9314041cfcac06abd598840be9388
PakAlyona <PakAlyona@users.noreply.github.com>
2019-02-16 10:42:40 +0600

    added creating directories

--- Week_2/Task_4/Program.cs
+++ Week_2/Task_4/Program.cs
@@ -8,11 +8,13 @@ namespace Task4
         
         public static void Main(string[] args) 
         {
-                StreamWriter task4 = new StreamWriter(@"C:/Texts/path1/Task4.txt");     // создаем файл в папке1
+            Directory.CreateDirectory("C:/Users/User/desktop/PP2/Week_2/Task_4/path1");
+            Directory.CreateDirectory("C:/Users/User/desktop/PP2/Week_2/Task_4/path2");
+            StreamWriter task4 = new StreamWriter(@"C:/Users/User/desktop/PP2/Week_2/Task_4/path1/Task4.txt");     // создаем файл в папке1
                 task4.WriteLine("Nadeus eto rabotaet");     // пишем туда текстик
                 task4.Close();
-                File.Copy(@"C:/Texts/path1/Task4.txt", @"C:/Texts/path2/Task4.txt");        // копируем из папки1 в папку2
-                File.Delete(@"C:/Texts/path1/Task4.txt");       // удаляем в папке1
+                File.Copy(@"C:/Users/User/desktop/PP2/Week_2/Task_4/path1/Task4.txt", @"C:/Users/User/desktop/PP2/Week_2/Task_4/path2/Task4.txt");        // копируем из папки1 в папку2
+                File.Delete(@"C:/Users/User/desktop/PP2/Week_2/Task_4/path1/Task4.txt");       // удаляем в папке1
 
         }
     }
